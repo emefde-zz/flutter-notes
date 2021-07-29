bbdaa084836d9dc1ddf4ebbc154518f2f028d599
Mateusz Fidos <mateuszfidos@gmail.com>
2021-07-29 10:27:44 +0200

    package:bloc core concepts done

--- dart-pad/dart_pad/bin/dart_pad.dart
+++ dart-pad/dart_pad/bin/dart_pad.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:bloc/bloc.dart';
 
 // void main(List<String> arguments) async {
@@ -28,12 +29,21 @@ Future<int> sumStream(Stream<int> stream) async {
 class CounterCubit extends Cubit<int> {
   CounterCubit(int initialState) : super(initialState);
 
-  void increment() => emit(state + 1);
+  void increment() {
+    addError(Exception('increment error!'), StackTrace.current);
+    emit(state + 1);
+  }
 
   @override
   void onChange(Change<int> change) {
     print(change);
     super.onChange(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error, $stackTrace');
+    super.onError(error, stackTrace);
   }
 }
 
@@ -59,8 +69,114 @@ final cubitB = CounterCubit(10); // 10 state cubit
 //   await cubid.close();
 // }
 
+// void main(List<String> args) {
+//   CounterCubit(0)
+//     ..increment()
+//     ..close();
+// }
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    print('${bloc.runtimeType} $change');
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('${bloc.runtimeType} $error $stackTrace');
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print('${bloc.runtimeType} $transition');
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    print('${bloc.runtimeType} $event');
+    super.onEvent(bloc, event);
+  }
+}
+
+// void main(List<String> args) {
+//   Bloc.observer = SimpleBlocObserver();
+//   CounterCubit(0)
+//     ..increment()
+//     ..close();
+
+//   CounterCubit(5)
+//     ..increment()
+//     ..close();
+// }
+
+enum CounterEvent { increment, error }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  CounterBloc() : super(0);
+
+  @override
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield state + 1;
+        break;
+      case CounterEvent.error:
+        addError(Exception('error!'), StackTrace.current);
+        yield state;
+        break;
+    }
+  }
+
+  @override
+  void onChange(Change<int> change) {
+    print(change);
+    super.onChange(change);
+  }
+
+  @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    print(transition);
+    super.onTransition(transition);
+  }
+
+  @override
+  void onEvent(CounterEvent event) {
+    print(event);
+    super.onEvent(event);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error, $stackTrace');
+    super.onError(error, stackTrace);
+  }
+}
+
+// Future<void> main(List<String> args) async {
+//   final bloc = CounterBloc();
+//   print(bloc.state);
+//   bloc.add(CounterEvent.increment);
+//   await Future.delayed(Duration.zero);
+//   print(bloc.state);
+//   await bloc.close();
+// }
+
+// Future<void> main(List<String> args) async {
+//   final bloc = CounterBloc();
+//   final subscription = bloc.stream.listen(print);
+//   bloc.add(CounterEvent.increment);
+//   await Future.delayed(Duration.zero);
+//   await subscription.cancel();
+//   await bloc.close();
+// }
+
 void main(List<String> args) {
-  CounterCubit(0)
-    ..increment()
+  Bloc.observer = SimpleBlocObserver();
+  CounterBloc()
+    ..add(CounterEvent.increment)
+    ..add(CounterEvent.error)
     ..close();
 }
