84e2baeac72e0f49cb36f49d64d410e9bd35d379
Mateusz Fidos <mateuszfidos@gmail.com>
2021-08-20 16:01:44 +0200

    some initial login tests

--- flutter_login/test/login/bloc/login_event_test.dart
+++ flutter_login/test/login/bloc/login_event_test.dart
@@ -1,1 +1,35 @@
+import 'package:flutter_login/login/bloc/login_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  group('Test LoginUsernameChanged', () {
+    test('is equal with same username', () {
+      expect(
+          LoginUsernameChanged('username'), LoginUsernameChanged('username'));
+    });
+
+    test('is not equal with different username', () {
+      expect(
+          LoginUsernameChanged('username') != LoginUsernameChanged('othername'),
+          true);
+    });
+  });
+
+  group('Test LoginPasswordChanged', () {
+    test('is equal with same username', () {
+      expect(
+          LoginPasswordChanged('password'), LoginPasswordChanged('password'));
+    });
+
+    test('is not equal with different passwords', () {
+      expect(LoginPasswordChanged('psswrd') != LoginPasswordChanged('password'),
+          true);
+    });
+  });
+
+  group('Test LoginSubmitted', () {
+    test('supports value comparison', () {
+      expect(LoginSubmitted(), LoginSubmitted());
+    });
+  });
+}
--- flutter_login/test/login/bloc/login_state_test.dart
+++ flutter_login/test/login/bloc/login_state_test.dart
@@ -1,1 +1,23 @@
+import 'package:flutter_login/login/bloc/login_bloc.dart';
+import 'package:flutter_login/login/models/models.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:formz/formz.dart';
 
+main() {
+  group('test LoginState', () {
+    test('supports value comparison', () {
+      expect(LoginState(), LoginState());
+    });
+
+    test('has proper values set', () {
+      final state = LoginState();
+      expect(state.status, FormzStatus.pure);
+      expect(state.password, Password.pure());
+      expect(state.username, Username.pure());
+    });
+
+    test('returns same object when no properties are passed', () {
+      expect(LoginState().copyWith(), LoginState());
+    });
+  });
+}
