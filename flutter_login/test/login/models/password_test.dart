3e1c686764bd7c797023b201563a471072ac78dd
Mateusz Fidos <mateuszfidos@gmail.com>
2021-08-21 12:20:55 +0200

    models tests

--- flutter_login/test/login/models/password_test.dart
+++ flutter_login/test/login/models/password_test.dart
@@ -1,1 +1,28 @@
-main() {}
+import 'package:flutter_login/login/login.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('test constructors', () {
+    test('test pure password', () {
+      final password = Password.pure();
+      expect(password.value, '');
+      expect(password.pure, true);
+    });
+
+    test('test dirty password', () {
+      final password = Password.dirty('password');
+      expect(password.value, 'password');
+      expect(password.pure, false);
+    });
+  });
+
+  group('valdiator tests', () {
+    test('return empty error when password empty', () {
+      expect(Password.dirty('').error, PasswordValidationError.empty);
+    });
+
+    test('no error when password not empty', () {
+      expect(Password.dirty('password').error, null);
+    });
+  });
+}
--- flutter_login/test/login/models/username_test.dart
+++ flutter_login/test/login/models/username_test.dart
@@ -1,1 +1,28 @@
-main() {}
+import 'package:flutter_login/login/login.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('test constructors', () {
+    test('test pure username', () {
+      final username = Username.pure();
+      expect(username.value, '');
+      expect(username.pure, true);
+    });
+
+    test('test dirty username', () {
+      final username = Username.dirty('username');
+      expect(username.value, 'username');
+      expect(username.pure, false);
+    });
+  });
+
+  group('valdiator tests', () {
+    test('return empty error when username empty', () {
+      expect(Username.dirty('').error, UsernameValidationError.empty);
+    });
+
+    test('no error when username not empty', () {
+      expect(Username.dirty('username').error, null);
+    });
+  });
+}
