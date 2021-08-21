87dff4dc0d6da7db6280be761428fec61e665800
Mateusz Fidos <mateuszfidos@gmail.com>
2021-08-21 12:49:46 +0200

    login view initial test and mocks

--- flutter_login/test/login/view/login_form_test.dart
+++ flutter_login/test/login/view/login_form_test.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_login/login/login.dart';
+
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
+class FakeLoginEvent extends Fake implements LoginEvent {}
+
+class FakeLoginState extends Fake implements LoginState {}
+
+@GenerateMocks([LoginBloc])
 main() {}
--- flutter_login/test/login/view/login_page_test.dart
+++ flutter_login/test/login/view/login_page_test.dart
@@ -1,1 +1,35 @@
-main() {}
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_login/login/login.dart';
+
+class MockAuthenticationRepository extends Mock
+    implements AuthenticationRepostiory {}
+
+main() {
+  late AuthenticationRepostiory authenticationRepostiory;
+
+  setUp(() {
+    authenticationRepostiory = MockAuthenticationRepository();
+  });
+
+  group('LoginPage tests', () {
+    test('LoginPage is routable', () {
+      expect(LoginPage.route(), isA<MaterialPageRoute>());
+    });
+
+    testWidgets(
+      "identify LoginPage",
+      (WidgetTester tester) async {
+        await tester.pumpWidget(RepositoryProvider.value(
+            value: authenticationRepostiory,
+            child: MaterialApp(
+              home: Scaffold(body: LoginPage()),
+            )));
+      },
+    );
+    expect(find.byType(LoginForm), findsOneWidget);
+  });
+}
