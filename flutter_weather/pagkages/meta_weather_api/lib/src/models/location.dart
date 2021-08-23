b40dcb379a83f371c38bcda6116fb17a6f3307be
Mateusz Fidos <mateuszfidos@gmail.com>
2021-08-23 10:03:59 +0200

    meta weather api and models

---
+++ flutter_weather/pagkages/meta_weather_api/lib/src/models/location.dart
@@ -0,0 +1,60 @@
+import 'package:json_annotation/json_annotation.dart';
+
+enum LocationType {
+  @JsonValue('City')
+  city,
+  @JsonValue('Region')
+  region,
+  @JsonValue('State')
+  state,
+  @JsonValue('Province')
+  province,
+  @JsonValue('Country')
+  country,
+  @JsonValue('Continent')
+  continent
+}
+
+@JsonSerializable()
+class Location {
+  Location({
+    required this.title,
+    required this.locationType,
+    required this.woeid,
+    required this.lattLong,
+  });
+
+  // factory Location.fromJson(Map<String, dynamic> json) =>
+  //     _$LocationFromJson(json);
+
+  final String title;
+  final LocationType locationType;
+  final int woeid;
+  @JsonKey(name: 'latt_long')
+  @LatLongConverter()
+  final LatLng lattLong;
+}
+
+class LatLng {
+  const LatLng({required this.latitude, required this.longitude});
+
+  final double latitude;
+  final double longitude;
+}
+
+class LatLongConverter implements JsonConverter<LatLng, String> {
+  const LatLongConverter();
+
+  @override
+  String toJson(LatLng latLng) {
+    return '${latLng.latitude},${latLng.longitude}';
+  }
+
+  @override
+  LatLng fromJson(String json) {
+    final split = json.split(',');
+    return LatLng(
+        latitude: double.tryParse(split.first) ?? 0,
+        longitude: double.tryParse(split.last) ?? 0);
+  }
+}
