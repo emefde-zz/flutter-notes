afd09e4f7bb7f3740b3066abd3f9b89b96c97ba5
emefde <mateuszfidos@gmail.com>
2021-09-05 17:27:46 +0200

    full logic done

--- flutter_weather/lib/weather/view/weather_page.dart
+++ flutter_weather/lib/weather/view/weather_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather/search/search.dart';
 import 'package:flutter_weather/settings/settings.dart';
 import 'package:flutter_weather/theme/cubit/theme_cubit.dart';
 import 'package:flutter_weather/weather/weather.dart';
--- flutter_weather/lib/weather/weather.dart
+++ flutter_weather/lib/weather/weather.dart
@@ -1,3 +1,7 @@
 export 'models/models.dart';
 export 'cubit/weather_cubit.dart';
 export 'view/weather_page.dart';
+export 'widgets/weather_empty.dart';
+export 'widgets/weather_error.dart';
+export 'widgets/weather_loading.dart';
+export 'widgets/weather_populated.dart';
